3f06ca7bd4fc298beb57c690a809d497c6889117
hatihatiadabot7-dev <hatihatiadabot7@gmail.com>
2026-06-18 11:03:48 +0700

    update

--- sipustaka/perpustakaan/models.py
+++ sipustaka/perpustakaan/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Buku(models.Model):
+    # Contoh kolom/field (sesuaikan dengan proyek kamu):
+    judul = models.CharField(max_length=200)
+    penulis = models.CharField(max_length=100)
+    
+    # Jika kamu menggunakan Raw SQL manual, kamu bisa mengunci nama tabelnya di sini:
+    class Meta:
+        db_table = 'perpustakaan_buku'
