27f79afcbc1b3ab218f9200b4787fad6d02b3a4d
hatihatiadabot7-dev <hatihatiadabot7@gmail.com>
2026-06-18 10:32:35 +0700

    update

--- sipustaka/sipustaka/settings.py
+++ sipustaka/sipustaka/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'sipustaka.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/home/aisyarkaanabdurrahman/PERPUSTAKAAN/sipustaka/db.sqlite3',
     }
 }
 
