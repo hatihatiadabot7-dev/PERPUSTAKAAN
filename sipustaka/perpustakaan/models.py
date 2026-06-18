9ed36f7dde7b2ac9d2fbff95d1c690db1c94137e
hatihatiadabot7-dev <hatihatiadabot7@gmail.com>
2026-06-18 13:36:58 +0700

    update

--- sipustaka/perpustakaan/models.py
+++ sipustaka/perpustakaan/models.py
@@ -1,5 +1,14 @@
-from django.db import models  # <-- Pastikan baris ini ada di paling atas!
+from django.db import models
 
+# 1. Model Buku (Jangan sampai hilang!)
+class Buku(models.Model):
+    judul = models.CharField(max_length=200)
+    penulis = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'perpustakaan_buku'
+
+# 2. Model Siswa (Tambahan baru)
 class Siswa(models.Model):
     nis = models.CharField(max_length=20, unique=True)
     nama = models.CharField(max_length=100)
