fd50fe8159c7c1acca5d61e1495a48bfebd12640
hatihatiadabot7-dev <hatihatiadabot7@gmail.com>
2026-06-18 13:46:02 +0700

    update

--- sipustaka/perpustakaan/models.py
+++ sipustaka/perpustakaan/models.py
@@ -16,3 +16,15 @@ class Siswa(models.Model):
 
     class Meta:
         db_table = 'perpustakaan_siswa'
+
+class Peminjaman(models.Model):
+    # Field standar untuk transaksi peminjaman:
+    buku = models.ForeignKey(Buku, on_delete=models.CASCADE)
+    siswa = models.ForeignKey(Siswa, on_delete=models.CASCADE)
+    tanggal_pinjam = models.DateField(auto_now_add=True)
+    tanggal_kembali = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=20, default='Dipinjam')
+
+    # Mengunci nama tabel agar terbaca oleh Raw SQL di views.py
+    class Meta:
+        db_table = 'perpustakaan_peminjaman'
